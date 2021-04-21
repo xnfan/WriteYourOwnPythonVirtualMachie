659024c0aea5dc6974bb6cf553448824a765bb0f
xnfan <xn_fan@163.com>
2021-04-22 04:18:19 +0800

    add tokenize.cpp

---
+++ LexicalAnalyzer_CH2/tokenize.cpp
@@ -0,0 +1,38 @@
+# include <stdio.h>
+# include <iostream>
+using namespace std;
+
+# define INIT 0
+# define NUM 1
+
+int main(){
+    FILE * fp = fopen("test_expression.txt", "r");
+    char ch;
+    int state, num = 0;
+
+    while ((ch = getc(fp)) != EOF){
+        if (ch == ' ' || ch == '\n'){
+            if(state == NUM){
+                printf("token NUM: %d\n", num);
+                state = INIT;
+                num = 0;
+            }
+        }
+        else if (ch >= '0' && ch <= '9'){
+            state = NUM;
+            num = num * 10 + ch - '0';
+        }
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')'){
+            if (state == NUM){
+                printf("token NUM: %d\n", num);
+                num = 0;
+            }
+            printf("token operator: %c\n", ch);
+            state = INIT;
+        }
+    }
+
+    fclose(fp);
+    return 0;
+
+}
---
+++ LexicalAnalyzer_CH2/tokenize_and_compute.py
@@ -0,0 +1,95 @@
+import sys
+import tokenize
+
+# tk = tokenize.generate_tokens(f.readline())
+# for token_num, token_value, _, _, _ in tk:   example that how to use tokenize
+
+# up-down
+# expression = term + term + ... + term
+# term = factor * factor * ... * factor
+# factor = num | (expression)
+# recursive
+
+
+class Token:
+    def __init__(self, token_num, token_value):
+        self.token_num = token_num
+        self.token_value = token_value
+
+
+global current_token
+
+
+def current():
+    global current_token
+    return current_token
+
+
+def next(tk):
+    token_num, token_value, _, _, _ = tk.__next__()
+    global current_token
+    current_token = Token(token_num, token_value)
+
+
+def expr(tk):
+    s1 = term(tk)
+    token_num, token_value = current().token_num, current().token_value
+
+    value = s1
+
+    while token_value == "+" or token_value == "-":
+        print("expr token is %s" % token_value)
+        next(tk)
+
+        s2 = term(tk)
+        if token_value == "+":
+            value += s2
+        else:
+            value -= s2
+        token_num, token_value = current().token_num, current().token_value
+    
+    print("expr value is %s" % value)
+    return value
+
+
+def term(tk):
+    f1 = factor(tk)
+    token_num, token_value = current().token_num, current().token_value
+
+    value = f1
+
+    while token_value == "*" or token_value == "/":
+        print("term token is %s" % token_value)
+        next(tk)
+
+        f2 = term(tk)
+        if token_value == '*':
+            value *= f2
+        else:
+            value /= f2
+        token_num, token_value = current().token_num, current().token_value
+    
+    print("term value is %s" % value)
+    return value
+
+
+def factor(tk):
+    value = 0
+    if current_token.token_num == tokenize.NUMBER:
+        value = current_token.token_value
+        next(tk)
+    elif current_token.token_value == "(":
+        next(tk)
+        f = expr(tk)
+        if current_token.token_value != ")":
+            print("parse error! value=%s" % current().token_value)
+        value = f
+        next(tk)
+    return int(value)
+
+
+if __name__ == '__main__':
+    with open(sys.argv[1], "r") as f:
+        tk = tokenize.generate_tokens(f.readline)
+        next(tk)
+        print(expr(tk))
